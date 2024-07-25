a45e06b60cc9563fed46d15a3617c6d78b46c868
JAYASIRI S.P.A.S. <ict21860@fot.sjp.ac.lk>
2024-07-26 01:17:50 +0530

    Setup

---
+++ ElectroCombat_Stopwatch/home.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ElectroCombat_Stopwatch
+{
+    public partial class home : Form
+    {
+        System.Timers.Timer timer;
+        int h, m, s, ms;
+
+        private void home_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+        }
+
+        private void home_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                timer.Start();
+            }
+        }
+
+        private void home_Load(object sender, EventArgs e)
+        {
+            timer = new System.Timers.Timer();
+            timer.Interval = 1;
+            timer.Elapsed += OnTimeEvent;
+        }
+
+        private void OnTimeEvent(object sender, EventArgs e)
+        {
+            Invoke(new Action(() =>
+            {
+                ms += 1;
+                if(ms == 63)
+                {
+                    ms = 0;
+                    s += 1;
+                }
+                if(s == 59)
+                {
+                    s = 0;
+                    m += 1;
+                }
+
+                watch_lb.Text = string.Format("{0}:{1}", m.ToString().ToString().PadLeft(2, '0'), s.ToString().ToString().PadLeft(2, '0'));
+            }));
+        }
+
+        public home()
+        {
+            InitializeComponent();
+        }
+    }
+}
